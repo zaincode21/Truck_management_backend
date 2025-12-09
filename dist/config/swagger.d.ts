ed661123e6f64b1474be7a2882de56b6ad118d6f
zaincode21 <sergedukuziyaremye@gmail.com>
2025-12-09 18:11:09 +0200

    remove modele

--- dist/config/swagger.d.ts
+++
@@ -1,4 +0,0 @@
-import swaggerUi from 'swagger-ui-express';
-export declare const specs: object;
-export { swaggerUi };
-//# sourceMappingURL=swagger.d.ts.map
--- dist/middleware/rateLimiter.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-/**
- * Rate Limiter Middleware
- */
-export declare function rateLimiter(windowMs?: number, // 15 minutes
-maxRequests?: number): (req: Request, res: Response, next: NextFunction) => void | Response<any, Record<string, any>>;
-//# sourceMappingURL=rateLimiter.d.ts.map
